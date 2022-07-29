1bf160a81aca50eb6f7ec78c7a0082b759204f38
Ben Whitfield <whitfieldbenjamin@gmail.com>
2022-07-29 19:49:38 -0400

    calculator exercise complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  return args.reduce((prevValue, nextValue) => prevValue + nextValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return args.reduce((prevValue, nextValue) => prevValue * nextValue);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let ans = 1;
+  for (let i = 0; i <= a; i++) {
+    if (i === 0) continue;
+    ans *= i
+  }
+  return ans;
 };
+
+// console.log(factorial(4));
 
 // Do not edit below this line
 module.exports = {
