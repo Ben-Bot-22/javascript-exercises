553723fbfd5a2d674b2a53228e8d14a34c638162
Ben Whitfield <whitfieldbenjamin@gmail.com>
2022-07-29 20:13:36 -0400

    Palindrome function complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    //convert to array
+    //remove all but letters and numbers
+    //compare first letter to last letter, second to second to last
+    let textChars = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s+/g, '').toLowerCase();
+    let textArr = [...textChars];
+    let isPalindrome = true;
+    // console.log(textChars);
+    for (let i = 0; i < textArr.length; i++)
+    {
+        // console.log(textArr[i] + " " + textArr[textArr.length - 1 - i]);
+        if (textArr[i] != textArr[textArr.length - 1 - i])
+        {
+            // console.log("FALSE: " + textArr[i] + " " + textArr[textArr.length - 1 - i]);
+            isPalindrome = false;
+        } 
+    }
+    return isPalindrome;
 };
+
+// console.log(palindromes('A car, a man, a maraca.'));
 
 // Do not edit below this line
 module.exports = palindromes;
