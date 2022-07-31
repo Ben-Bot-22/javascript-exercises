a8e5586bbbbce49e024d7f5069a8d5b4f2f75026
Ben Whitfield <whitfieldbenjamin@gmail.com>
2022-07-31 17:34:06 -0400

    fibonacci exercise complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(seq) {
+    if (seq < 0) return "OOPS";
+    let fib = [];
+    fib.push(1);
+    fib.push(1);
+    for (let i = 0; i < seq; i++) {
+        fib[i + 2] = fib[i] + fib[1 + i];
+    }
+    return fib[seq-1];
 };
 
 // Do not edit below this line
