f65172cfd29291fb944aef4af380ccb2a01ffe15
Ben Whitfield <whitfieldbenjamin@gmail.com>
2022-07-31 19:05:20 -0400

    findTheoldest.js complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const year = new Date().getFullYear();
+  //get age of the people in the array
+  const age = people.map(function(person) {
+    let deathDate = year;
+    if (person.yearOfDeath) {
+        deathDate = person.yearOfDeath;
+    }
+    return deathDate - person.yearOfBirth;
+  });
+  //get index of highest number in array
+  const max = Math.max(...age);
+  const index = age.indexOf(max);
+  return people[index];
 };
 
 // Do not edit below this line
